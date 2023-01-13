308e160a9210da00c65252e6d41bd64bc22d73f1
Osman Gulaid <116044508+Oszzy@users.noreply.github.com>
2023-01-13 08:49:45 +0000

    Add files via upload

---
+++ website.js
@@ -0,0 +1,8 @@
+'use strict';
+(function logger(apple, orange) {
+  const juice = `jusice with ${apple} apple and ${orange} oranges`;
+  console.log(juice)
+  return juice
+})(6, 8);
+
+logger(apple, orange)
