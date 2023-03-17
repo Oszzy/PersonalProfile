95efdb647f0e241d03fe757678e23c7a0f6d6d94
Osman Gulaid <116044508+Oszzy@users.noreply.github.com>
2023-03-17 19:59:45 +0000

    Delete website.js

--- website.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-(function logger(apple, orange) {
-  const juice = `jusice with ${apple} apple and ${orange} oranges`;
-  console.log(juice)
-  return juice
-})(6, 8);
-
-logger(apple, orange)
